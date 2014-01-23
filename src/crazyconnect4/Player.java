db56c17b283e1b79879a2ab56e4a7da90ab944a4
dccoatney <dccoatney@192.168.1.124>
2014-01-23 13:22:26 -0800

    Crystal Coatney made a change

--- src/crazyconnect4/Player.java
+++ src/crazyconnect4/Player.java
@@ -28,5 +28,45 @@ public class Player {
     public void displayName() {
         System.out.println("\t\tGreetings, my name is "  + this.name); 
     }
+    
+    public String getPlayerStastics() {
+        String playerStatistics = 
+                this.name + " has won "
+                + this.getWinningPercentage(this.wins, this.losses, this.ties) + "% of the games."
+                + "\n\t" + this.wins + " wins, "
+                + this.losses + " losses and "
+                + this.ties + " ties.";
+        return playerStatistics;
 
+}
+    public double getWinningPercentage(long wins, long losses, long ties) {
+     
+        if (wins < 0 ) {
+            System.out.println("\n\tThe number of wins must be "
+                    + "greater than or equal to zero.");
+            return -999;
+        }
+        
+        if (losses < 0 ) {
+            System.out.println("\n\tThe number of losses must be "
+                    + "greater than or equal to zero.");
+            return -999;
+        }
+        
+        if (ties < 0 ) {
+            System.out.println("\n\tThe number of ties must be "
+                    + "greater than or equal to zero.");
+            return -999;
+        }
+        
+        double totalScore = wins + losses + ties;
+        
+        if (totalScore ==  0) {
+            return 0;
+        }
+        
+        double winLossRatio = wins / totalScore;
+        return winLossRatio * 100;
+    }
+    
 }
